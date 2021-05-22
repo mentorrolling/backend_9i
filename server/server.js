3aa9a57c23ea63a8eb5bb4889a5dede67858fe38
Pablo Marino <pmarino2013@gmail.com>
2021-05-21 21:08:28 -0300

    Rutas usuario, login, jwt

--- server/server.js
+++ server/server.js
@@ -1,30 +1,32 @@
-const express = require('express')
-const app = express()
- 
-app.use(express.urlencoded({
-    extended: true
-}));
+const express = require("express");
+const mongoose = require("mongoose");
+require("./config/config");
 
+const app = express();
 
-app.get('/usuarios', function (req, res) {
-  res.json('GET usuarios')
-})
-app.post('/usuarios', function (req, res) {
-    // res.json('POST usuarios')
-    let body = req.body;
-
-    res.json({
-        body
-    })
-
+// app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: true,
   })
-  app.put('/usuarios/:id', function (req, res) {
-    res.json('PUT usuarios')
-  })
-  app.delete('/usuarios/:id', function (req, res) {
-    res.json('DELETE usuarios')
-  })
- 
-app.listen(3004, ()=>{
-    console.log('Escuchando en puerto 3004')
-})
+);
+
+app.use(require("./rutas/index"));
+
+mongoose.connect(
+  "mongodb://localhost:27017/academy",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  },
+  (err, res) => {
+    if (err) throw err;
+    console.log("Base de datos online");
+  }
+);
+
+app.listen(process.env.PORT, () => {
+  console.log("Escuchando en puerto", process.env.PORT);
+});
